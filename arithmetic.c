896893f356b3edb3cfdc1fdb2cee62d8244655bc
zamirul <javshazib@gmail.com>
2026-04-05 21:22:11 +0600

    division learning

--- arithmetic.c
+++ arithmetic.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main(){
-    int a =5;
-    int b=7;
+    int a =10;
+    int b=2;
     int sum =a+b;
     printf("sumation of %d and %d is %d\n",a,b,sum);
     int sub= a-b;
@@ -10,5 +10,7 @@ int main(){
 
     int mul =a* b;
     printf("multiplication of %d and %d is %d\n",a,b,mul);
+    int div = a / b;
+    printf("division of  is %d\n",div);
     return 0;
 }
