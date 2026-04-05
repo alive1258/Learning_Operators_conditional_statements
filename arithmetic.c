cae5f3fa7d05a96bebdeb482fc033c8ff7f2549e
zamirul <javshazib@gmail.com>
2026-04-05 21:19:01 +0600

    sumation and subtraction laening

--- arithmetic.c
+++ arithmetic.c
@@ -4,6 +4,8 @@ int main(){
     int a =5;
     int b=7;
     int sum =a+b;
-    printf("%d\n",sum);
+    printf("sumation of %d and %d is %d\n",a,b,sum);
+    int sub= a-b;
+    printf("subtraction of %d and %d is %d\n",a,b,sub);
     return 0;
 }
