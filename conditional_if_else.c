762a9051f5af0a5321e8cc0be418c60dfd9ca75a
zamirul <javshazib@gmail.com>
2026-04-05 22:41:48 +0600

    if else learning

---
+++ conditional_if_else.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int tk;
+    scanf("%d",&tk);
+
+    if(tk >=100){
+        printf(" i eat burgur\n");
+    }
+    else{
+        printf("i don't eat burger\n");
+    }
+}
