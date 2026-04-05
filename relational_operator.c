10a3ffccf1a65fe3c86677675f01fdfabef4d044
zamirul <javshazib@gmail.com>
2026-04-05 21:42:10 +0600

    relation opearation learn

---
+++ relational_operator.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+    // raltion opearators
+    int a =5;
+    int b=2;
+    if(a>b){
+        printf("%d is greater than %d\n",a,b);
+    }
+    if(a<b){
+        printf("%d is less than %d\n",a,b);
+    }
+    if(a==b){
+        printf("%d is equal to %d\n",a,b);
+    }
+    if(a!=b){
+        printf("%d is not equal to %d\n",a,b);
+    }
+    return 0;
+
+}
