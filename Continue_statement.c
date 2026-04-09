719bf0e44754475f125a1532bcf2c977da9ea665
zamirul <javshazib@gmail.com>
2026-04-09 08:03:10 +0600

    Continue statement

---
+++ Continue_statement.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    for (int i = 1; i <= 10; i++)
+    {
+        if (i == 3)
+        {
+
+            continue;
+        }
+
+        printf("%d -Even\n", i);
+    }
+    return 0;
+}
