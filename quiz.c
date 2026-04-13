ff92c0755acdab6ca4396afdc84c064692df6c7f
zamirul <javshazib@gmail.com>
2026-04-13 06:31:38 +0600

    quiz

--- quiz.c
+++ quiz.c
@@ -1,11 +1,139 @@
-#include <stdio.h>
+        #include <stdio.h>
 
-int main(){
-int i = 5;
-while(i<10)
-{
-    printf("i");
-    i++;
-}
+        int main(){
+        int x;
+        scanf("%d", &x);
+        if(x%2!=0)
+        {
+            printf("Odd ");
+        }
+
+        else{
+            printf("Even ");
+        }
+        if(x>0){
+            printf("Positive ");
+        }
+        else{
+            printf("Negative ");
+        }
+
+        return 0;
+
+        
+        }
+
+#include <stdio.h>
+ int main(){
+   int x=0;
+   if(x==1){
+    if(x==0){
+        printf("inside if if\n");
+    }else{
+        printf("inside if else\n");
+    }
+   }
+   else{
+    printf("inside else\n");
+   }
+return 0;
+
+} 
+
+#include <stdio.h>
+ int main(){
+   int marks=76;
+   if(marks>=80){
+    printf("A+");
+   }else if(marks>=70){
+    printf("A");
+  }else if(marks>=70){
+    printf("A-");
+  }
+return 0;
+
+} 
+
+// What is the output of the following code snippet?
+#include <stdio.h>
+ int main(){
+   int num =5;
+   if(num >10){
+    printf("Greater than 10");
+   }else if (num<10)
+   {
+    printf("Less than 10");
+   }else{
+    printf("Equal to 10");
+   }
    
-}
+return 0;
+
+} 
+
+#include <stdio.h>
+ int main(){
+   int marks;
+    scanf("%d", &marks);
+    if(marks>=80){
+     printf("A+");
+    }
+    if(marks>=70){
+        printf("A");
+    }
+      if(marks>=70){
+        printf("A-");
+    }
+return 0;
+
+} 
+
+#include <stdio.h>
+ int main(){
+   int x=0;
+   if(x==1){
+    if(x>=0){
+        printf("true\n");
+    }else{
+        printf("false\n");
+    }
+   }
+return 0;
+
+} 
+
+
+#include <stdio.h>
+ int main(){
+   int x=5;
+   int y=4;
+   if(x>=y && x<y+4){
+    if(y<5){
+        printf("hi ");
+    }else if(y<10){
+        printf("hi ");
+    }
+   }
+   else{
+    printf("Hello ");
+   }
+return 0;
+
+} 
+#include <stdio.h>
+ int main(){
+   int a=5;
+    int b=1;
+    int c=3;
+    if(a>b && b>c){
+        printf("%d ", a);
+    }
+    else if(b>a && c>b){
+        printf("%d ", b);
+    }
+    else {
+        printf("%d ", c);
+    }
+return 0;
+
+} 
