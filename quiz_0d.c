29e49d963cc835031bb9c2f3ab5084cce7f1ecba
zamirul <javshazib@gmail.com>
2026-04-15 08:03:15 +0600

    Reverse printing of an array

---
+++ quiz_0d.c
@@ -0,0 +1,29 @@
+// #include <stdio.h>
+// int main()
+// {
+//     for (int i = 1; i <= 10; i++)
+//     {
+//         printf("hi ");
+//         if (i > 2)
+//         {
+//             break;
+//         }
+//     }
+//     return 0;
+
+#include <stdio.h>
+int main()
+{
+    for (int i = 0; i <= 10; i++)
+    {
+
+        if (i > 2)
+        {
+            break;
+            ;
+        }
+        printf("hi \n");
+    }
+
+    return 0;
+}
